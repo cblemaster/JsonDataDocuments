3f487e35c5ed76258745f3d84cdacb3c0c3282f9
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-27 10:39:01 -0400

    added login page

---
+++ JsonDataDocuments/Pages/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace JsonDataDocuments.Pages;
+
+public partial class LoginPage : Page
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    private void YesButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+
+    }
+
+    private void NoButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+
+    }
+}
