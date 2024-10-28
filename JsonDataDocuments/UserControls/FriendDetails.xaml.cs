d3c4d5b248eac3b2361fc3d019fe524adddeb1b2
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-28 17:27:59 -0400

    added friend details user control

---
+++ JsonDataDocuments/UserControls/FriendDetails.xaml.cs
@@ -0,0 +1,50 @@
+﻿using JsonDataDocuments.Models;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JsonDataDocuments.UserControls;
+
+public partial class FriendDetails : UserControl, INotifyPropertyChanged
+{
+    private string _friendName = string.Empty;
+    private Color _favoriteColor;
+
+    public string FriendName
+    {
+        get => _friendName;
+        set
+        {
+            if (!value.Equals(_friendName))
+            {
+                _friendName = value;
+                PropertyChanged!(this, new PropertyChangedEventArgs(nameof(FriendName)));
+            }
+        }
+    }
+    public Color FavoriteColor
+    {
+        get => _favoriteColor;
+        set
+        {
+            if (!value.Equals(_favoriteColor))
+            {
+                _favoriteColor = value;
+                PropertyChanged!(this, new PropertyChangedEventArgs(nameof(FavoriteColor)));
+            }
+            
+        }
+    }
+    
+    public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+
+    public FriendDetails() => InitializeComponent();
+
+    private void UserControl_Loaded(object sender, RoutedEventArgs e)
+    {
+        var friend = new Friend() { Id = 1, Name = "brian", FavoriteColor = Color.Blue };
+        FriendName = friend.Name;
+        FavoriteColor = friend.FavoriteColor;
+    }
+}
