e233347ae9e8c2edc20c868992947fb1ea73a32c
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-28 10:10:26 -0400

    added models for friend and available colors

---
+++ JsonDataDocuments/Models/FavColorOptions.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace JsonDataDocuments.Models;
+
+public static class FavColorOptions
+{
+    public static Color[] AllColors => [Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Purple,
+        Color.Gray, Color.Brown, Color.Black, Color.White, Color.Pink];
+}
---
+++ JsonDataDocuments/Models/Friend.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace JsonDataDocuments.Models;
+
+public class Friend
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public Color FavoriteColor { get; set; }
+    
+    public override string ToString() => $"Id: {Id}|Name: {Name}|Favorite Color: {FavoriteColor}";
+}
