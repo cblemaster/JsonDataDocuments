b372df97305927669becfc2e564e027865408beb
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-28 10:11:10 -0400

    added string length validation rule

---
+++ JsonDataDocuments/Validation/StringLengthRule.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace JsonDataDocuments.Validation;
+
+public class StringLengthRule : ValidationRule
+{
+    public int Min { get; set; }
+    public int Max { get; set; }
+    public string PropName { get; set; } = string.Empty;
+
+    public StringLengthRule() { }
+
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        string s = (string)value;
+        return (s.Length < Min) || (s.Length > Max)
+            ? new ValidationResult(false, $"{PropName} must be between {Min} and {Max} characters.")
+            : ValidationResult.ValidResult;
+    }
+}
