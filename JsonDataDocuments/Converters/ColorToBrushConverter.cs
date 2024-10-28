ac9fd93a68bdf93a2e53020c9ffb2602deb4a138
Brian LeMaster <brian.lemaster@outlook.com>
2024-10-28 17:27:37 -0400

    added binding value converter

---
+++ JsonDataDocuments/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace JsonDataDocuments.Converters;
+
+internal class ColorToBrushConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is System.Drawing.Color c)
+        {
+            return new System.Windows.Media.SolidColorBrush(new System.Windows.Media.Color() { A = c.A, B = c.B, G = c.G, R = c.R });
+        }
+        return value;
+    }
+    
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
